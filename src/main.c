bdfead0e676a29f844cb22454568c1366425e42a
Albin <albinnykvist03@gmail.com>
2025-08-21 11:57:42 +0200

    improvements to viewCollections + exporting to textfile added

--- src/main.c
+++ src/main.c
@@ -6,8 +6,12 @@
 #include <direct.h>
 #include <io.h>
 #include <ctype.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
 #define FILE_PATH_LENGTH 60
+
+#define BUFFER_SIZE 1024 // used for exporting to textfile
 
 const char* NEW_NOTES_FOLDER = "../note_data/new_notes";
 const char* COLLECTIONS_FOLDER = "../note_data/collections";
@@ -530,6 +534,49 @@ void reviewNewNotes() {
     }
 }
 
+int removeDirectory(const char *path) {
+    DIR *dir = opendir(path);
+    if (!dir) {
+        perror("opendir");
+        return -1;
+    }
+
+    struct dirent *entry;
+    char filepath[1024];
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            continue;
+
+        snprintf(filepath, sizeof(filepath), "%s/%s", path, entry->d_name);
+
+        struct stat statbuf;
+        if (stat(filepath, &statbuf) == 0) {
+            if (S_ISDIR(statbuf.st_mode)) {
+                removeDirectory(filepath);
+            } else {
+                if (remove(filepath) != 0) {
+                    perror("remove file");
+                }
+            }
+        }
+    }
+    closedir(dir);
+
+    if (rmdir(path) != 0) {
+        perror("rmdir");
+        return -1;
+    }
+
+    return 0;
+}
+
+int deleteCollection(const char * collectionName) {
+    char path[FILE_PATH_LENGTH] = ""; 
+    snprintf(path, sizeof(path), "%s/%s", COLLECTIONS_FOLDER, collectionName);
+    return removeDirectory(path);
+}
+
 // used by editcollection and viewcollections
 void printCollection(const char * collectionName) {
     int numNotes = getNumNotes(collectionName);
@@ -547,6 +594,92 @@ void printCollection(const char * collectionName) {
     free(notes);
 }
 
+void printViewCollectionsPrompt(struct _finddata_t * collections, int numCollections) {
+    if(numCollections <= 0) {
+        printf("no collections found\n");
+        printf("[create: c + <collection name>]>");
+    }else {
+        for(int i=0; i<numCollections; i++) {
+            printf("[%d] %s (#%d)\n", i+1, collections[i].name, getNumNotes(collections[i].name));
+        }
+        printf("[print: p + <number> | delete: d + <number> | export to textfile: e + <number> | create: c + <collection name>]>");
+    }
+}
+
+void updateCollections(struct _finddata_t ** collections, int * numCollections) {
+    *numCollections = getNumCollections();
+
+    if (*collections) {
+        free(*collections);
+        *collections = NULL;
+    }
+
+    *collections = malloc(*numCollections * sizeof(struct _finddata_t));
+    if (!*collections) {
+        perror("malloc failed");
+        *numCollections = 0;
+        return;
+    }
+
+    if (!getCollections(collections, *numCollections)) {
+        printf("failed to get collections\n");
+    }
+}
+
+
+
+int exportCollection(const char *collectionName, const char *exportFile) {
+    FILE *out = fopen(exportFile, "w");
+    if (!out) {
+        perror("fopen exportFile");
+        return -1;
+    }
+
+    // Build search pattern for files in collection
+    char collectionPath[FILE_PATH_LENGTH] = "";
+    snprintf(collectionPath, sizeof(collectionPath), "%s/%s", COLLECTIONS_FOLDER, collectionName);
+    char searchPath[260];
+    snprintf(searchPath, sizeof(searchPath), "%s\\*.*", collectionPath);
+
+    struct _finddata_t fileinfo;
+    intptr_t hFile = _findfirst(searchPath, &fileinfo);
+    if (hFile == -1L) {
+        perror("findfirst");
+        fclose(out);
+        return -1;
+    }
+
+    fprintf(out, "==== %s ====\n\n", collectionName);
+
+    do {
+        if (fileinfo.attrib & _A_SUBDIR) continue;
+
+        char filepath[260];
+        snprintf(filepath, sizeof(filepath), "%s\\%s", collectionPath, fileinfo.name);
+
+        FILE *in = fopen(filepath, "r");
+        if (!in) {
+            fprintf(out, "Failed to open note: %s\n", fileinfo.name);
+            continue;
+        }
+
+        char buffer[BUFFER_SIZE];
+        size_t n;
+        while ((n = fread(buffer, 1, sizeof(buffer), in)) > 0) {
+            fwrite(buffer, 1, n, out);
+        }
+        fprintf(out, "\n\n");
+
+        fclose(in);
+
+    } while (_findnext(hFile, &fileinfo) == 0);
+
+    _findclose(hFile);
+    fclose(out);
+
+    return 0;
+}
+
 // Print the collections list and ask what the user wants to do with a given collection.
 // Note Collections:
 // [1] Chemistry, 32 notes
@@ -561,15 +694,7 @@ void viewCollections() {
     }
 
     // print prompt to user
-    if(numCollections <= 0) {
-        printf("no collections found\n");
-        printf("[create <new collection name>]>");
-    }else {
-        for(int i=0; i<numCollections; i++) {
-            printf("[%d] %s (#%d)\n", i+1, collections[i].name, getNumNotes(collections[i].name));
-        }
-        printf("[print: p + <number> | delete: d + <number> | edit: e + <number> | save as textfile: s + <number> | create <new collection name>]>");
-    }
+    printViewCollectionsPrompt(collections, numCollections);
 
     // get input from user
     char input[MAX_INPUT_LENGTH];
@@ -581,52 +706,60 @@ void viewCollections() {
             continue;
         }
 
-        if(isdigit(input[0])) { // User attempts to choose a listed collection
-
-            int inputNumber = stringToInteger(input);
-            if(inputNumber == -1 || inputNumber > numCollections) {
-                printf("invalid input\n>");
-                continue;
-            }
-
-            break;
-
-        }else if(input[0] == 'p' || input[0] == 'd' || input[0] == 'e' || input[0] == 's') {
-
-            int inputNumber = extractFirstInteger(input);
-
+        int inputNumber = extractFirstInteger(input);   // this does not work for creating new collections
+        if(input[0] != 'c') {
             if(inputNumber <= 0 || inputNumber > numCollections) {
                 printf("invalid input\n>");
                 continue;
             }
+        }
 
-            switch(input[0])
-            {
-            case 'p':
-                printCollection(collections[inputNumber - 1].name);
-                break;
-            case 'd':
-                break;
-            case 'e':
-                break;
-            case 's':
-                break;
-            
-            default: // print
-                printCollection(collections[inputNumber - 1].name);
-                break;
+        switch(input[0])
+        {
+        case 'p':
+            printf("printing...\n");
+            printCollection(collections[inputNumber - 1].name);
+            break;
+        case 'd':
+            printf("delting...\n");
+            if (deleteCollection(collections[inputNumber - 1].name) == 0) {
+                printf("Deleted collection.\n");
+            } else {
+                printf("Failed to delete collection.\n");
             }
-            break; // exit loop
+            break;
+        case 'e':
+            printf("exporting...\n");
+            if(exportCollection(collections[inputNumber - 1].name, "export.txt") == 0) {
+                 printf("Export successful!\n");
+            } else {
+                printf("Export failed.\n");
+            }
+            break;
+        case 'c':
+            printf("creating...\n");
+            char *ptr = input + 1;
+            while (*ptr == ' ' || *ptr == '\t')
+                ptr++;
 
-        }else { // User attempts to create and save the note to a new collection
-
-            if(!createNewCollection(input)) {
+            char newName[FILE_PATH_LENGTH];
+            strncpy(newName, ptr, FILE_PATH_LENGTH - 1);
+            newName[FILE_PATH_LENGTH - 1] = '\0'; 
+            if(!createNewCollection(newName)) {
                 printf("failed to create new collection\n>");
                 continue;
             }
-
+            break;
+        case 'q':
+            printf("quitting...\n");
+            exit(0);  
+            break;
+        default: // print
+            printCollection(collections[inputNumber - 1].name);
             break;
         }
+        updateCollections(&collections, &numCollections);
+        printViewCollectionsPrompt(collections, numCollections);
     }
 
     free(collections);
